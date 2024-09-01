9ed292a2b107c41ce02ad966f4c6f8f1c93d2dad
Temuulen <temuulenkhuchit01@gmail.com>
2024-08-31 21:00:27 -0500

    added test case on 13th problem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        RomanToInteger13 romanNumber = new RomanToInteger13();
+        System.out.println(romanNumber.romanToInt("MCMXCIV"));
+
     }
 }
--- src/RomanToInteger13.java
+++ src/RomanToInteger13.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class RomanToInteger13 {
+    public int romanToInt(String s) {
+        int answer = 0, value;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currLetter = s.charAt(i);
+            int currNumber = letterToNumber(currLetter);
+
+            if (i + 1 < s.length()) {
+                char nextLetter = s.charAt(i + 1);
+                int nextNumber = letterToNumber(nextLetter);
+
+                if (nextNumber > currNumber) {
+                    value = currNumber * -1;
+                } else {
+                    value = currNumber;
+                }
+            } else {
+                value = currNumber;
+            }
+            answer += value;
+        }
+
+        return answer;
+    }
+
+    public int letterToNumber(char letter) {
+        return switch (letter) {
+            case 'M' -> 1000;
+            case 'D' -> 500;
+            case 'C' -> 100;
+            case 'L' -> 50;
+            case 'X' -> 10;
+            case 'V' -> 5;
+            case 'I' -> 1;
+            default -> 0;
+        };
+    }
 }
