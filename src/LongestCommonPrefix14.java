da66162e55dc5c0820f58374da2f8487030d9f85
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-01 22:37:36 -0500

    Solved LongestCommonPrefix14.java problem

---
+++ src/LongestCommonPrefix14.java
@@ -0,0 +1,31 @@
+public class LongestCommonPrefix14 {
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder prefix = new StringBuilder();
+        String minString = strs[0];
+
+        for (String str : strs) {
+            if (str.length() < minString.length()) {
+                minString = str;
+            }
+        }
+
+        for (int i = 0; i < minString.length(); i++) {
+            boolean isSimilar = true;
+            char letter = strs[0].charAt(i);
+            if (strs.length > 1) {
+                for (String str : strs) {
+                    if (letter != str.charAt(i)) {
+                        isSimilar = false;
+                        break;
+                    }
+                }
+            }
+            if (isSimilar)
+                prefix.append(letter);
+            else
+                break;
+        }
+
+        return prefix.toString();
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        RomanToInteger13 romanNumber = new RomanToInteger13();
-        System.out.println(romanNumber.romanToInt("MCMXCIV"));
-
+//        RomanToInteger13 romanNumber = new RomanToInteger13();
+//        System.out.println(romanNumber.romanToInt("MCMXCIV"));
+        String[] strs = {"flower","flow","flight"};
+        String[] strs2 = {"a"};
+        System.out.println(LongestCommonPrefix14.longestCommonPrefix(strs));
+        System.out.println(LongestCommonPrefix14.longestCommonPrefix(strs2));
     }
 }
