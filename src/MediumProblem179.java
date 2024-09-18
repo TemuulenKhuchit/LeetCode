281d716e3e15d9b7f8699b036deac6992b33aa27
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-18 00:46:49 -0500

    Solved Problem 179

---
+++ src/MediumProblem179.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MediumProblem179 {
+    public String largestNumber(int[] nums) {
+        List<Integer> list = Arrays.stream(nums).boxed().toList();
+
+        List<String> strList = list.stream()
+                .map(String::valueOf)
+                .collect(Collectors.toList());
+
+        strList.sort((a, b) -> (b + a).compareTo(a + b));
+
+        if (strList.get(0).equals("0")) {
+            return "0";
+        }
+
+        return String.join("", strList);
+    }
+
+    public static void main(String[] args) {
+        var a = new MediumProblem179();
+        int[] nums = {0, 0};
+        System.out.println(a.largestNumber(nums));
+    }
+}
