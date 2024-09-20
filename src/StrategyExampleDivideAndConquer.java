0ae69efc0cd26ee5593bc0bcefdc5eb1c32ea07e
Temuulen <temuulenkhuchit01@gmail.com>
2024-09-19 20:26:20 -0500

    StrategyExampleDivideAndConquer

--- src/StrategyExampleDivideAndConquer.java
+++ src/StrategyExampleDivideAndConquer.java
@@ -1,24 +1,30 @@
 public class StrategyExampleDivideAndConquer {
-    static int count = 0;
+    static int twoWayCounter = 0;
+    static int threeWayCounter = 0;
+
     public static void main(String[] args) {
         int[] array = {200, 3, 42, -1561, 6, 7, 89, 595458, 156, 516, 155, -156, 516};
-        System.out.println("Max value is: " + findMax(array));
-        System.out.println("Function called " + count + " times.");
+
+        System.out.println("Two way search's Max value is: " + findMax(array));
+        System.out.println("Two way search's Function called " + twoWayCounter + " times.");
     }
 
-    public static int findMax(int[] arr) {
-        if (arr == null)
+    public static int findMax(int[] array) {
+        if (array == null)
             return 0;
-        return findMaxRecursive(0, arr.length - 1, arr);
+
+        return findMaxRecursive(0, array.length - 1, array);
     }
 
-    public static int findMaxRecursive(int i, int j, int[] fullArray) {
-        StrategyExampleDivideAndConquer.count++;
+    public static int findMaxRecursive(int i, int j, int[] array) {
         if (i == j)
-            return fullArray[i];
+            return array[i];
+
+        StrategyExampleDivideAndConquer.twoWayCounter++;
         int mid = (i + j) / 2;
+
         return Math.max(
-                findMaxRecursive(i, mid, fullArray),
-                findMaxRecursive(mid + 1, j, fullArray));
+                findMaxRecursive(i, mid, array),
+                findMaxRecursive(mid + 1, j, array));
     }
 }
