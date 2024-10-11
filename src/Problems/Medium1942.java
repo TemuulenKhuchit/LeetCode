8048cba0f432051cf86087cc047a98d75bdc1beb
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-10 23:31:49 -0500

    Medium 1942

---
+++ src/Problems/Medium1942.java
@@ -0,0 +1,19 @@
+package Problems;
+
+public class Medium1942 {
+
+    public static void main(String[] args) {
+        smallestChair(new int[][]{{1, 4}, {2, 3}, {4, 6}}, 1);
+        smallestChair(new int[][]{{3, 10}, {1, 5}, {2, 6}}, 0);
+    }
+
+    public static int smallestChair(int[][] times, int targetFriend) {
+        int result = 0;
+
+
+
+        System.out.println(result);
+        return result;
+    }
+
+}
