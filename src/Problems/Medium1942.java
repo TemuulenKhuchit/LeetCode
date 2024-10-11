f4d4f0a5fba11c0c93e4746f50f8159820a28f67
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-11 18:06:02 -0500

    Medium 1942

--- src/Problems/Medium1942.java
+++ src/Problems/Medium1942.java
@@ -10,20 +10,43 @@ public class Medium1942 {
     }
 
     public static int smallestChair(int[][] times, int targetFriend) {
-        int result = 0;
-        int[] target = times[targetFriend];
+        int n = times.length;
 
-        Arrays.sort(times, Comparator.comparing(n -> n[0]));
-
-        Queue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i <= target[0]; i++) {
-//            if (times[])
+        int[][] friends = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            friends[i][0] = times[i][0]; // arrival time
+            friends[i][1] = times[i][1]; // leaving time
+            friends[i][2] = i;           // friend index
         }
 
-        System.out.println(Arrays.deepToString(times));
+        Arrays.sort(friends, Comparator.comparingInt(a -> a[0]));
 
-        return result;
+        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            availableChairs.add(i);
+        }
+
+        PriorityQueue<int[]> occupiedChairs = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+
+        for (int[] friend : friends) {
+            int arrival = friend[0];
+            int leaving = friend[1];
+            int index = friend[2];
+
+            while (!occupiedChairs.isEmpty() && occupiedChairs.peek()[0] <= arrival) {
+                availableChairs.add(occupiedChairs.poll()[1]);
+            }
+
+            int assignedChair = availableChairs.poll();
+
+            if (index == targetFriend) {
+                return assignedChair;
+            }
+
+            occupiedChairs.add(new int[]{leaving, assignedChair});
+        }
+
+        return -1;
     }
 
 }
