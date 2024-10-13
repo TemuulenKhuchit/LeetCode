1d45f7b0ae2be0e2adb2f4fde71cc17d13bfedc0
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-12 21:59:21 -0500

    Contest419

--- src/Weekly/Contest419/Question1.java
+++ src/Weekly/Contest419/Question1.java
@@ -1,4 +1,29 @@
 package Weekly.Contest419;
 
+import java.util.Arrays;
+
 public class Question1 {
+
+    public static void main(String[] args) {
+        findXSum(new int[]{1, 1, 2, 2, 3, 4, 2, 3}, 6, 2);
+    }
+
+    public static int[] findXSum(int[] nums, int k, int x) {
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        for (int i = 0; i < result.length; i++) {
+            int count = 0;
+            while (count < k) {
+                result[i] += nums[i + count];
+                System.out.print(nums[i + count] + ", ");
+                count++;
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.toString(result));
+        return result;
+    }
+
 }
