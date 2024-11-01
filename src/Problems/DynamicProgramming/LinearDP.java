e97ee414bd62476c11e568913aadff1b548b9d77
Temuulen <temuulenkhuchit01@gmail.com>
2024-11-01 10:21:53 -0500

    Easy 1137. N-th Tribonacci Number

--- src/Problems/DynamicProgramming/LinearDP.java
+++ src/Problems/DynamicProgramming/LinearDP.java
@@ -6,7 +6,8 @@ public class LinearDP {
         LinearDP ldp = new LinearDP();
 
 //        ldp.climbStairs(3);
-        ldp.fib(4);
+//        ldp.fib(25);
+        ldp.tribonacci(25);
 //        ldp.minCostClimbingStairs(new int[]{10, 15, 20});
     }
 
@@ -35,6 +36,23 @@ public class LinearDP {
 
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    // Easy 1137. N-th Tribonacci Number
+    public int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
         }
 
         return dp[n];
