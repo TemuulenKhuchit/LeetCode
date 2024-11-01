707985688833432fc38f2563f2944f164ceaf118
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-31 19:04:17 -0500

    Easy 509. Fibonacci Number

--- src/Problems/DynamicProgramming/LinearDP.java
+++ src/Problems/DynamicProgramming/LinearDP.java
@@ -5,15 +5,16 @@ public class LinearDP {
     public static void main(String[] args) {
         LinearDP ldp = new LinearDP();
 
-        ldp.climbStairs(3);
+//        ldp.climbStairs(3);
+        ldp.fib(4);
+//        ldp.minCostClimbingStairs(new int[]{10, 15, 20});
     }
 
-    // Easy 70
+    // Easy 70. Climbing Stairs
     public int climbStairs(int n) {
         if (n == 1) return 1;
 
         int[] dp = new int[n + 1];
-
         dp[1] = 1;
         dp[2] = 2;
 
@@ -23,4 +24,25 @@ public class LinearDP {
 
         return dp[n];
     }
+
+    // Easy 509. Fibonacci Number
+    public int fib(int n) {
+        if (n == 0) return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    // Easy 746. Min Cost Climbing Stairs
+//    public int minCostClimbingStairs(int[] cost) {
+//
+//    }
+
 }
