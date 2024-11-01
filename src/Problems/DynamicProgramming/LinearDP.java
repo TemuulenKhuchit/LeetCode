3ebe9f97a1a3ddb2f4a53517e1ae74860ecda447
Temuulen <temuulenkhuchit01@gmail.com>
2024-11-01 18:37:40 -0500

    Easy 121. Best Time to Buy and Sell Stock

--- src/Problems/DynamicProgramming/LinearDP.java
+++ src/Problems/DynamicProgramming/LinearDP.java
@@ -1,5 +1,7 @@
 package Problems.DynamicProgramming;
 
+import java.util.Arrays;
+
 public class LinearDP {
 
     public static void main(String[] args) {
@@ -7,7 +9,8 @@ public class LinearDP {
 
 //        ldp.climbStairs(3);
 //        ldp.fib(25);
-        ldp.tribonacci(25);
+//        ldp.tribonacci(25);
+        ldp.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
 //        ldp.minCostClimbingStairs(new int[]{10, 15, 20});
     }
 
@@ -52,15 +55,31 @@ public class LinearDP {
         dp[2] = 1;
 
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
 
         return dp[n];
     }
 
+    // Easy 121. Best Time to Buy and Sell Stock
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 1) return 0;
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                dp[j] = Math.max(prices[j - 1] - prices[i], Math.max(dp[j], dp[j - 1]));
+
+        return dp[n];
+    }
+
     // Easy 746. Min Cost Climbing Stairs
-//    public int minCostClimbingStairs(int[] cost) {
-//
-//    }
+    public int minCostClimbingStairs(int[] cost) {
+
+
+        return 0;
+    }
 
 }
