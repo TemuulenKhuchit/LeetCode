f5bf279de8d7f75784ee982d1aaf92663ff91bd5
Temuulen <temuulenkhuchit01@gmail.com>
2024-11-20 01:18:51 -0600

    Medium695

--- src/Problems/Medium695.java
+++ src/Problems/Medium695.java
@@ -32,19 +32,22 @@ public class Medium695 {
         for (int row = 0; row < m; row++)
             for (int col = 0; col < n; col++)
                 if (grid[row][col] == 1) {
-                    bfs(grid, row, col, 0); // BFS solution
-                    dfs(grid, row, col, 0); // DFS with stack solution
-                    dfsRec(grid, row, col, 0); // DFS with recursive solution
+//                    bfs(grid, row, col); // BFS solution
+//                    dfs(grid, row, col); // DFS with stack solution
+//                    max = Math.max(max, dfsRec(grid, row, col)); // DFS with recursive solution
+                    max = Math.max(max, dfsRec2(grid, row, col)); // DFS with recursive fastest solution
                 }
 
+        System.out.println(max);
         return max;
     }
 
     // BFS solution
-    public void bfs(int[][] grid, int row, int col, int currSize) {
+    public void bfs(int[][] grid, int row, int col) {
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{row, col});
 
+        int area = 0;
         grid[row][col] = 0;
 
         while (!queue.isEmpty()) {
@@ -60,15 +63,16 @@ public class Medium695 {
                 }
             }
 
-            max = Math.max(max, ++currSize);
+            max = Math.max(max, ++area);
         }
     }
 
     // DFS with stack solution
-    public void dfs(int[][] grid, int row, int col, int currSize) {
+    public void dfs(int[][] grid, int row, int col) {
         Stack<int[]> stack = new Stack<>();
         stack.push(new int[]{row, col});
 
+        int area = 0;
         grid[row][col] = 0;
 
         while (!stack.isEmpty()) {
@@ -84,24 +88,39 @@ public class Medium695 {
                 }
             }
 
-            max = Math.max(max, ++currSize);
+            max = Math.max(max, ++area);
         }
     }
 
     // DFS with recursive solution
-    public int dfsRec(int[][] grid, int row, int col, int currSize) {
+    public int dfsRec(int[][] grid, int row, int col) {
+        int area = 1;
         grid[row][col] = 0;
-
-        max = Math.max(max, ++currSize);
 
         for (int[] dir : dirs) {
             int newRow = row + dir[0];
             int newCol = col + dir[1];
 
             if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && grid[newRow][newCol] == 1)
-                currSize = dfsRec(grid, newRow, newCol, currSize);
+                area += dfsRec(grid, newRow, newCol);
         }
 
-        return currSize;
+        return area;
+    }
+
+    // DFS with recursive fastest solution
+    public int dfsRec2(int[][] grid, int row, int col) {
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == 0)
+            return 0;
+
+        grid[row][col] = 0;
+
+        int area = 1;
+        area += dfsRec2(grid, row + 1, col);
+        area += dfsRec2(grid, row - 1, col);
+        area += dfsRec2(grid, row, col + 1);
+        area += dfsRec2(grid, row, col - 1);
+
+        return area;
     }
 }
