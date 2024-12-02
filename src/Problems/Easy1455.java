44a6b2610ffc4f48e11efec1c196ede961789f7a
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-02 15:54:47 -0600

    Easy1455

---
+++ src/Problems/Easy1455.java
@@ -0,0 +1,21 @@
+package Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Easy1455 {
+
+    public static void main(String[] args) {
+        isPrefixOfWord("i love eating burger", "burg");
+    }
+
+    // Easy 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+    public static int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++)
+            if (words[i].startsWith(searchWord))
+                return i + 1;
+        return -1;
+    }
+
+}
