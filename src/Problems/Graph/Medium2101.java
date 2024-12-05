4b5f39e4615668e8d0c5c36b2a9d8d7bd5a72931
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-04 20:19:37 -0600

    Medium2101

--- src/Problems/Graph/Medium2101.java
+++ src/Problems/Graph/Medium2101.java
@@ -2,6 +2,7 @@ package Problems.Graph;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Medium2101 {
 
@@ -20,7 +21,8 @@ public class Medium2101 {
         int count = 0;
 
         for (int i = 0; i < bombs.length; i++)
-            count = Math.max(count, bfs(bombs, i));
+//            count = Math.max(count, bfs(bombs, i)); // BFS solution
+            count = Math.max(count, dfs(bombs, i)); // DFS with stack solution
 
         return count;
     }
@@ -61,5 +63,37 @@ public class Medium2101 {
     }
 
     // DFS with stack solution
+    public static int dfs(int[][] bombs, int bombInd) {
+        int count = 0;
+        int[] counted = new int[bombs.length];
 
+        Stack<Integer> stack = new Stack<>();
+        stack.push(bombInd);
+
+        while (!stack.isEmpty()) {
+            int currBombInd = stack.pop();
+
+            count++;
+            counted[currBombInd] = 1;
+
+            int x1 = bombs[currBombInd][0];
+            int y1 = bombs[currBombInd][1];
+            int r1 = bombs[currBombInd][2];
+
+            for (int i = 0; i < bombs.length; i++) {
+                if (currBombInd == i)
+                    continue;
+
+                int x2 = bombs[i][0];
+                int y2 = bombs[i][1];
+
+                double dist = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+
+                if (dist <= r1 && counted[i] == 0 && !stack.contains(i))
+                    stack.push(i);
+            }
+        }
+
+        return count;
+    }
 }
