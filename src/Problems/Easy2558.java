b5a1bd50fa539187a7633fef806db903f8abd684
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-11 19:40:59 -0600

    Easy2558

---
+++ src/Problems/Easy2558.java
@@ -0,0 +1,28 @@
+package Problems;
+
+import java.util.Arrays;
+
+public class Easy2558 {
+    public static void main(String[] args) {
+        System.out.println(pickGifts(new int[]{25, 64, 9, 4, 100}, 4));
+    }
+
+    // Easy 2558. Take Gifts From the Richest Pile
+    public static long pickGifts(int[] gifts, int k) {
+        while (k > 0) {
+            int maxIndex = 0;
+            for (int i = 0; i < gifts.length; i++) {
+                if (gifts[i] > gifts[maxIndex])
+                    maxIndex = i;
+            }
+            gifts[maxIndex] = (int) Math.floor(Math.sqrt(gifts[maxIndex]));
+            k--;
+        }
+
+        long sum = 0;
+        for (int i = 0; i < gifts.length; i++)
+            sum += gifts[i];
+
+        return sum;
+    }
+}
