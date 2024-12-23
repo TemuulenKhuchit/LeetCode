b9d6d433c3a8265cc57f465fd41dd4e057e59852
Temuulen <temuulenkhuchit01@gmail.com>
2024-12-22 22:27:37 -0600

    Easy1

---
+++ src/Problems/ArraysAndHashing/Easy1.java
@@ -0,0 +1,26 @@
+package Problems.ArraysAndHashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Easy1 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{3, 2, 4}, 6)));
+    }
+
+    // Easy 1. Two Sum
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i]))
+                return new int[]{i, map.get(target - nums[i])};
+            map.put(nums[i], i);
+        }
+
+        return new int[2];
+    }
+
+}
