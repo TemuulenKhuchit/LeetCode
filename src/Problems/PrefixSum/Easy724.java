eba18ea290fc25e727e6c3ce095d0199d87e3453
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-03 17:51:52 -0600

    Easy724

--- src/Problems/PrefixSum/Easy724.java
+++ src/Problems/PrefixSum/Easy724.java
@@ -10,17 +10,16 @@ public class Easy724 {
     public static int pivotIndex(int[] nums) {
         int n = nums.length;
 
-        int[] left_prefix = new int[n + 1];
-        for (int i = 0; i < n; i++)
-            left_prefix[i + 1] = left_prefix[i] + nums[i];
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
 
-        int[] right_prefix = new int[n + 1];
-        for (int i = n - 1; i >= 0; i--)
-            right_prefix[i] = right_prefix[i + 1] + nums[i];
-
-        for (int i = 0; i < n; i++)
-            if (left_prefix[i] == right_prefix[i + 1])
+        int leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            if (leftSum == sum - leftSum - nums[i])
                 return i;
+            leftSum += nums[i];
+        }
 
         return -1;
     }
