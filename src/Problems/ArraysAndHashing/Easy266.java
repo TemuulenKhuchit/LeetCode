15b9a85c972f4b56f38ce9494f2b1db7081a389c
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-14 10:20:58 -0600

    Easy266

---
+++ src/Problems/ArraysAndHashing/Easy266.java
@@ -0,0 +1,25 @@
+package Problems.ArraysAndHashing;
+
+public class Easy266 {
+
+    public static void main(String[] args) {
+        System.out.println(canPermutePalindrome("code"));
+    }
+
+    // Easy 266. Palindrome Permutation
+    public static boolean canPermutePalindrome(String s) {
+        int[] letters = new int[26];
+        for (char c : s.toCharArray())
+            letters[c - 'a']++;
+        boolean hasFoundOdd = false;
+        for (int letter : letters) {
+            if (letter % 2 == 1) {
+                if (hasFoundOdd)
+                    return false;
+                hasFoundOdd = true;
+            }
+        }
+        return true;
+    }
+
+}
