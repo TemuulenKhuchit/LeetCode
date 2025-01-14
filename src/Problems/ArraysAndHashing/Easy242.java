76b1ca8b302ce87eee8968876f5b8cfe1c757f7a
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-13 23:21:29 -0600

    Easy242

--- src/Problems/ArraysAndHashing/Easy242.java
+++ src/Problems/ArraysAndHashing/Easy242.java
@@ -14,6 +14,33 @@ public class Easy242 {
 
     // Easy 242. Valid Anagram
     public static boolean isAnagram(String s, String t) {
+        int[] arr = new int[26];
+        for (char c : s.toCharArray())
+            arr[c - 'a']++;
+        for (char c : t.toCharArray())
+            arr[c - 'a']--;
+        for (int num : arr)
+            if (num != 0)
+                return false;
+        return true;
+    }
+
+    public static boolean isAnagram2(String s, String t) {
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (char c : s.toCharArray())
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        for (char c : t.toCharArray()) {
+            if (!sMap.containsKey(c))
+                return false;
+            if (sMap.get(c) == 1)
+                sMap.remove(c);
+            else
+                sMap.put(c, sMap.get(c) - 1);
+        }
+        return sMap.isEmpty();
+    }
+
+    public static boolean isAnagram3(String s, String t) {
         Map<Character, Integer> sMap = s.chars()
                 .mapToObj(ch -> (char) ch)
                 .collect(Collectors.toMap(ch -> ch, ch -> 1, Integer::sum));
@@ -34,23 +61,6 @@ public class Easy242 {
             }
         }
 
-        return true;
-    }
-
-    public static boolean isAnagram2(String s, String t) {
-        Map<Character, Integer> sMap = new HashMap<>();
-        for (char c : s.toCharArray())
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
-        for (char c : t.toCharArray()) {
-            if (!sMap.containsKey(c))
-                return false;
-            if (sMap.get(c) == 1)
-                sMap.remove(c);
-            else
-                sMap.put(c, sMap.get(c) - 1);
-        }
-        if (!sMap.isEmpty())
-            return false;
         return true;
     }
 
