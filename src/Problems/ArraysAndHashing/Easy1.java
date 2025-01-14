6147b604b166e3d2afa1855d1185cceeae46afaf
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-14 15:55:33 -0600

    Easy1

--- src/Problems/ArraysAndHashing/Easy1.java
+++ src/Problems/ArraysAndHashing/Easy1.java
@@ -13,13 +13,11 @@ public class Easy1 {
     // Easy 1. Two Sum
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i]))
+            if (map.containsKey(target - nums[i]))
                 return new int[]{i, map.get(target - nums[i])};
             map.put(nums[i], i);
         }
-
         return new int[2];
     }
 
