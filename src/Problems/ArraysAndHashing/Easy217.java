ff1add324680d8cfacf3ce184750c872c331e76d
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-13 23:21:15 -0600

    Easy217

--- src/Problems/ArraysAndHashing/Easy217.java
+++ src/Problems/ArraysAndHashing/Easy217.java
@@ -13,15 +13,16 @@ public class Easy217 {
 
     // Easy 217. Contains Duplicate
     public static boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-        return nums.length != set.size();
-    }
-
-    public static boolean containsDuplicate2(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums)
             if (!set.add(num))
                 return true;
         return false;
     }
+
+    public static boolean containsDuplicate2(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return nums.length != set.size();
+    }
+
 }
