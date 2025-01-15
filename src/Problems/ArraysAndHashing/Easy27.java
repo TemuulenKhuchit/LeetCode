b44eadf561cefd61fa13d3d4c57f011dc17de258
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-15 15:52:47 -0600

    Easy27

---
+++ src/Problems/ArraysAndHashing/Easy27.java
@@ -0,0 +1,23 @@
+package Problems.ArraysAndHashing;
+
+public class Easy27 {
+
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+    }
+
+    // Easy 27. Remove Element
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            }
+            else i++;
+        }
+        return n;
+    }
+
+}
