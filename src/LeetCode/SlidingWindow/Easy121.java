b3d38a094e605bb36bc0f717cbc2f0e2af53a6f9
Temuulen <temuulenkhuchit01@gmail.com>
2025-01-30 00:57:53 -0600

    Easy121

---
+++ src/LeetCode/SlidingWindow/Easy121.java
@@ -0,0 +1,22 @@
+package LeetCode.SlidingWindow;
+
+public class Easy121 {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    // Easy 121. Best Time to Buy and Sell Stock
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice)
+                minPrice = price;
+            else if (price - minPrice > maxProfit)
+                maxProfit = price - minPrice;
+        }
+        return maxProfit;
+    }
+
+}
