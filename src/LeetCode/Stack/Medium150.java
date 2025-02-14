094ecad531b665753a00ed88a74e6b8ae26ed317
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-14 00:41:07 -0600

    Medium150

---
+++ src/LeetCode/Stack/Medium150.java
@@ -0,0 +1,32 @@
+package LeetCode.Stack;
+
+import java.util.Stack;
+
+public class Medium150 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Medium 150. Evaluate Reverse Polish Notation
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if(!"+-*/".contains(token)) {
+                stack.push(Integer.parseInt(token));
+                continue;
+            }
+            int x = stack.pop();
+            int y = stack.pop();
+            int ans = switch (token) {
+                case "+" -> y + x;
+                case "-" -> y - x;
+                case "*" -> y * x;
+                case "/" -> y / x;
+                default -> 0;
+            };
+            stack.push(ans);
+        }
+        return stack.pop();
+    }
+}
