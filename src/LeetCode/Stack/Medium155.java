181dcd2463431d49607a4b87d1bab81c7427d7fe
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-14 00:26:29 -0600

    Medium155

---
+++ src/LeetCode/Stack/Medium155.java
@@ -0,0 +1,34 @@
+package LeetCode.Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Medium155 {
+
+    private int size = -1;
+    private List<Integer> list = new ArrayList<>();
+    private List<Integer> minValList = new ArrayList<>();
+
+    public void push(int val) {
+        if (minValList.isEmpty() || minValList.get(size) > val)
+            minValList.add(val);
+        else minValList.add(minValList.get(size));
+        list.add(val);
+        size++;
+    }
+
+    public void pop() {
+        list.remove(size);
+        minValList.remove(size);
+        size--;
+    }
+
+    public int top() {
+        return list.get(size);
+    }
+
+    public int getMin() {
+        return minValList.get(size);
+    }
+
+}
