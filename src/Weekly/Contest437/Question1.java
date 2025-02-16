ce020609c8cacf8bf98c206de7d6777b438a4b1b
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-15 23:08:51 -0600

    Weekly Contest 437 Question1

---
+++ src/Weekly/Contest437/Question1.java
@@ -0,0 +1,35 @@
+package Weekly.Contest437;
+
+public class Question1 {
+    // For testing the solution
+    public static void main(String[] args) {
+        System.out.println(hasSpecialSubstring("aabaa", 2));
+    }
+
+    public static boolean hasSpecialSubstring(String s, int k) {
+        int n = s.length();
+        if (k > n)
+            return false;
+
+        int i = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            int j = i;
+            while (j < n && s.charAt(j) == c)
+                j++;
+            int count = j - i;
+            if (count == k) {
+                if (i > 0 && s.charAt(i - 1) == c) {
+                }
+                else if (j < n && s.charAt(j) == c) {
+                }
+                else {
+                    return true;
+                }
+            }
+            i = j;
+        }
+        return false;
+    }
+
+}
