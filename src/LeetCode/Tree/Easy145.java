1d7769659143eee1ff6053fd236e986a51205137
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-23 23:35:19 -0600

    Easy145

---
+++ src/LeetCode/Tree/Easy145.java
@@ -0,0 +1,23 @@
+package LeetCode.Tree;
+
+public class Easy145 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Easy 145. Binary Tree Postorder Traversal
+
+}
