b9f41c2e745ee114997e6b835909fc52f1faefe9
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-24 23:41:04 -0600

    Easy226

---
+++ src/LeetCode/Tree/Easy226.java
@@ -0,0 +1,31 @@
+package LeetCode.Tree;
+
+public class Easy226 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Easy 226. Invert Binary Tree
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
