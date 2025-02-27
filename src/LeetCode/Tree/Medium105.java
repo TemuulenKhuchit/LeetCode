a32612e1e2f86d471d6ab00cfc9ffe7099b0f872
Temuulen <temuulenkhuchit01@gmail.com>
2025-02-26 21:56:55 -0600

    Medium105

---
+++ src/LeetCode/Tree/Medium105.java
@@ -0,0 +1,49 @@
+package LeetCode.Tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Medium105 {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Medium 105. Construct Binary Tree from Preorder and Inorder Traversal
+    private int preorderIndex = 0;
+    private Map<Integer, Integer> inorderIndexMap = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++)
+            inorderIndexMap.put(inorder[i], i);
+        return arrayToTree(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode arrayToTree(int[] preorder, int left, int right) {
+        if (left > right)
+            return null;
+
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+
+        return root;
+
+        // Preorder: [3, 9, 20, 15, 7]
+        // Inorder:  [9, 3, 15, 20, 7]
+    }
+}
